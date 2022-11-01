7a0864a990f9b157778b8c3c06a7a8c0961c6ad5
panupongth148 <97428822+panupongth148@users.noreply.github.com>
2022-11-01 19:13:54 +0700

    update: dropdown should resource

--- pg-pool-ui/src/app/assign-resource/assign-resource.component.ts
+++ pg-pool-ui/src/app/assign-resource/assign-resource.component.ts
@@ -11,51 +11,74 @@ import ResourceModel from '../shared/interface/ResourceModel';
 })
 export class AssignResourceComponent implements OnInit {
 
-  constructor(private projectHttpRequestService:ProjectHttpRequestService, private resourceHttpRequestService:ResourceHttpRequestService) { }
-  projectRequest?:any[]
-  dataListResource?:any[]
-  resources?:ResourceModel[]
-  resourceLReq?:any[]
-  resourcesEmpty?:ResourceModel[]
+  constructor(private projectHttpRequestService: ProjectHttpRequestService, private resourceHttpRequestService: ResourceHttpRequestService) { }
+  projectRequest?: any[]
+  dataListResource?: any[] = []
+  resources?: ResourceModel[]
+  resourceLReq?: any[] = [];
+  resourcesEmpty?: ResourceModel[]
+  addResourceConfirm?: any[] = []
+  positionSelected: any[] = []
+  startDateList:any[] = []
+  endDateList:any[] = []
+  durationList:any[] = []
+  assignList:any[] = []
+  reqAssign?: any[] = []
+  projects?:any[] = []
   ngOnInit(): void {
     this.getProjectHaveRequest()
     this.getAllResource()
-    
+
   }
 
-  getProjectHaveRequest(){
+  getProjectHaveRequest() {
     this.projectHttpRequestService.getProjectHaveRequest().subscribe(res => {
-     console.log(res)
-     this.projectRequest = res
+      // console.log(res)
+      this.projectRequest = res
 
-  })
+
+    })
   }
-  getAllResource(){
-    this.resourceHttpRequestService.getAllResource().subscribe((res)=>{
-      
+  async getAllResource() {
+    this.resourceHttpRequestService.getAllResource().subscribe((res) => {
+
       this.resources = res
-      console.log(res)
-      this.resourcesEmpty = res.filter((val)=>{
+      this.resourcesEmpty = res.filter((val) => {
         return val.projects == null
       })
+      this.filterResourceEmpty()
     })
-    this.filterResourceEmpty()
+
   }
-  filterResourceEmpty(){
-    this.projectRequest?.forEach(val =>{
-      val.projectRequest.forEach((reqestRe:any) =>{
-        this.resourcesEmpty?.forEach(resource =>{
-          if(resource.position == reqestRe.positionRequest){
-            this.resourceLReq?.push(resource)
+  filterResourceEmpty() {
+    this.projectRequest?.forEach(val => {
+      // console.log(val)
+      val.requests.forEach((reqestRe: any) => {
+        this.resourcesEmpty?.forEach(resource => {
+          // console.log(resource)
+          // console.log(reqestRe.positionRequest)
+          if (resource.position == reqestRe.positionRequest) {
+            this.resourceLReq?.push({ label: resource.prefix + " " + resource.firstName + " " + resource.lastName + "     Position : " + resource.position, resource: resource })
+            // console.log("match")
+            // console.log(this.resourceLReq)
           }
         })
-        
+
       })
-      this.dataListResource?.push({projectCode: val.projectCode, resourceL:this.resourceLReq})
-      this.resourceLReq=[]
+
+      this.dataListResource?.push({ projectCode: val.projectCode, resourceL: this.resourceLReq })
+      console.log(this.dataListResource)
+      this.resourceLReq = []
     })
-    console.log(this.dataListResource)
+    // console.log(this.dataListResource)
   }
 
-
+  addResource(projectId: any, index: any) {
+    console.log(index)
+    console.log(this.positionSelected[index])
+    this.projects?.push({assigned:this.assignList[index], duration: this.durationList[index], startDate:this.startDateList[index], endDate:this.endDateList[index], working:(this.assignList[index]*100)/this.durationList[index]})
+    console.log(this.projects)
+    this.addResourceConfirm?.push({resourceId: this.positionSelected[index].resource.id , projects:this.projects})
+    console.log(this.addResourceConfirm)
+  }
 }
