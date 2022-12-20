2a692e80083f1c103c544c9b7ccabb893032345e
panupongth148 <97428822+panupongth148@users.noreply.github.com>
2022-12-21 06:56:52 +0700

    update: ui

--- pg-pool-ui/src/app/register/register.component.ts
+++ pg-pool-ui/src/app/register/register.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { UserHttpRequestService } from '../service/user/user-http-request.service';
 import RegisterModel from '../shared/interface/RegisterModel';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  providers: [MessageService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private userHttpRequestService:UserHttpRequestService) { }
+  constructor(private userHttpRequestService:UserHttpRequestService, private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
@@ -24,6 +26,7 @@ export class RegisterComponent implements OnInit {
     const register = this.registerForm.value as RegisterModel
     console.log(register)
     this.userHttpRequestService.registerUser(register).subscribe(val=>{
+      this.messageService.add({severity:'success', summary: 'Success', detail: 'สมัครสมาชิกสำเร็จ'});
       console.log(val)
     })
   }
